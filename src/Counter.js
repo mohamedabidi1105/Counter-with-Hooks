66cb470274021e6d500cba9f4a93ced53d4fc3cd
ABIDI Mohamed <mohamed.abidi1105@gmail.com>
2020-05-26 10:32:22 +0200

    update counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 
 function Counter() {
-  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
-  // Déclare une fonction setCount qui permet de modifier la valeur de count
+
   const [count, setCount] = useState(0);
 
   return (
